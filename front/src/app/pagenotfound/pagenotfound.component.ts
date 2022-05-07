b9354e7cdd50ec3002b985d24fa3a13c506b5321
Antoine Pascual <antoinepascualmail@gmail.com>
2022-05-07 18:13:23 +0200

    Récupération du lien sur la page 404

--- front/src/app/pagenotfound/pagenotfound.component.ts
+++ front/src/app/pagenotfound/pagenotfound.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router,NavigationEnd  } from '@angular/router';
 
 @Component({
   selector: 'app-pagenotfound',
@@ -6,13 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagenotfound.component.css']
 })
 export class PagenotfoundComponent implements OnInit {
+  url: string[];
   getSessionInfos(){
     return sessionStorage.getItem('name');
   }
   logout() {
     sessionStorage.clear();
   }
-  constructor() { }
+  constructor(public http: HttpClient, router: Router) {
+    //console.log(router.url);
+    const url = router.url.split(',');
+    this.url = url
+
+
+    // link de la page = this.url
+    
+  }
 
   ngOnInit(): void {
   }
